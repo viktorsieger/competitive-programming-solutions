662037a71ea69b15635bca32c3ec03cd8abb45bc
viktorsieger <23069803+viktorsieger@users.noreply.github.com>
2020-06-19 23:59:25 +0200

    Solved nodup

---
+++ kattis/nodup/nodup.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    string word;
+    unordered_set<string> words;
+    bool isRepeated = false;
+
+    while(cin >> word) {
+
+        auto search = words.find(word);
+
+        if(search == words.end()) {
+            words.insert(word);
+        }
+        else {
+            isRepeated = true;
+            break;
+        }
+    }
+
+    cout << (isRepeated ? "no" : "yes") << '\n';
+
+    return 0;
+}
